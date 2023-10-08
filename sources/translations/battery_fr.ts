43586782a9bad7925d2c90a1abc567093d4acda6
GustavLindberg99 <95423695+GustavLindberg99@users.noreply.github.com>
2023-10-08 15:01:23 +0200

    Create battery_fr.ts

---
+++ sources/translations/battery_fr.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="55"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="59"/>
+        <source>Warn me when the battery is higher than</source>
+        <translation>Afficher un message quand ne niveau de la batterie est supérieur à</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="61"/>
+        <source>Check this checkbox to get a warning when the battery is charging and its percentage is more than the indicated percentage.</source>
+        <translation>Cochez cette case pour obtenir un avertissement quand votre ordinateur est branché et que le niveau de la batterie est supérieur au niveau indiqué.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="75"/>
+        <source>Warn me when the battery is lower than</source>
+        <translation>Afficher un message quand ne niveau de la batterie est inférieur à</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="77"/>
+        <source>Check this checkbox to get a warning when the battery is not charging and its percentage is less than the indicated percentage.</source>
+        <translation>Cochez cette case pour obtenir un avertissement quand votre ordinateur n&apos;est pas branché et que le niveau de la batterie est inférieur au niveau indiqué.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="92"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="93"/>
+        <source>Closes this window and saves the settings.</source>
+        <translation>Ferme cette fenètre et enregistre les paramètres.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="100"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="103"/>
+        <source>Closes this window without saving the settings.</source>
+        <translation>Ferme cette fenêtre sans enregistrer les paramètres.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="114"/>
+        <source>%1% charged</source>
+        <translation>%1% chargé</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="130"/>
+        <source>Battery</source>
+        <translation>Batterie</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="120"/>
+        <source>The battery is almost fully charged (%1%).</source>
+        <translation>Votre niveau de batterie est élevée (%1%).</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="124"/>
+        <source>The battery is running low (%1%).</source>
+        <translation>Votre niveau de batterie est faible (%1%).</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="144"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Paramètres</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="164"/>
+        <source>&amp;About</source>
+        <translation>À &amp;propos de ce programme</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="169"/>
+        <source>About</source>
+        <translation>À propos de ce programme</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Battery warning program by Gustav Lindberg.</source>
+        <translation>Programme d&apos;avertissement de batterie par Gustav Lindberg.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Icons made by %3 from %1 are licensed by %2.</source>
+        <translation>Les icônes faites par %3 pour %1 sont sous license de %2.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Source code:</source>
+        <translation>Code source:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>This program is licensed under the GNU GPL 3.0.</source>
+        <translation>Ce programme est licensié sous GNU GPL 3.0.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="173"/>
+        <source>About &amp;Qt</source>
+        <translation>À propos de &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="177"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="181"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="updates.cpp" line="26"/>
+        <source>Installing updates...</source>
+        <translation>Installation des mises à jour...</translation>
+    </message>
+    <message>
+        <location filename="updates.cpp" line="196"/>
+        <source>An update is available. Do you want to install it now?</source>
+        <translation>Une mise à jour est disponible. Voulez-vous l&apos;installer maintenant?</translation>
+    </message>
+</context>
+</TS>
