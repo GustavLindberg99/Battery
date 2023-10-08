fdb884b8f035d75b39cdab6c1c23280af3fd12e9
GustavLindberg99 <95423695+GustavLindberg99@users.noreply.github.com>
2023-10-08 15:01:41 +0200

    Add files via upload

---
+++ sources/translations/battery_sv.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="55"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="59"/>
+        <source>Warn me when the battery is higher than</source>
+        <translation>Visa en varning när batteriet är mer laddat än</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="61"/>
+        <source>Check this checkbox to get a warning when the battery is charging and its percentage is more than the indicated percentage.</source>
+        <translation>Kryssa den här rutan för att få en varning när batteriet är mer laddat än det angivna värdet.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="75"/>
+        <source>Warn me when the battery is lower than</source>
+        <translation>Visa en varning när batteriet är mindre laddat än</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="77"/>
+        <source>Check this checkbox to get a warning when the battery is not charging and its percentage is less than the indicated percentage.</source>
+        <translation>Kryssa den här rutan för att få en varning när batteriet är mindre laddat än det angivna värdet.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="92"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="93"/>
+        <source>Closes this window and saves the settings.</source>
+        <translation>Stänger fönstret och sparar inställningarna.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="100"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="103"/>
+        <source>Closes this window without saving the settings.</source>
+        <translation>Stänger fönstret utan att spara inställningarna.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="114"/>
+        <source>%1% charged</source>
+        <translation>%1% laddat</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="130"/>
+        <source>Battery</source>
+        <translation>Batteri</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="120"/>
+        <source>The battery is almost fully charged (%1%).</source>
+        <translation>Batteriet är nästan fulladdat (%1%).</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="124"/>
+        <source>The battery is running low (%1%).</source>
+        <translation>Batteriet är nästan urladdat (%1%).</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="144"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Inställningar</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="164"/>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="169"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Battery warning program by Gustav Lindberg.</source>
+        <translation>Batterivarningsprogram av Gustav Lindberg.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Icons made by %3 from %1 are licensed by %2.</source>
+        <translation>Ikonerna gjorda av %3 från %1 är licensierade enligt %2.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>Source code:</source>
+        <translation>Källkod:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="170"/>
+        <source>This program is licensed under the GNU GPL 3.0.</source>
+        <translation>Detta program är licensierat enligt GNU GPL 3.0.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="173"/>
+        <source>About &amp;Qt</source>
+        <translation>Om &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="177"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjälp</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="181"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <location filename="updates.cpp" line="26"/>
+        <source>Installing updates...</source>
+        <translation>Installerar uppdateringar...</translation>
+    </message>
+    <message>
+        <location filename="updates.cpp" line="196"/>
+        <source>An update is available. Do you want to install it now?</source>
+        <translation>En uppdatering är tillgänglig. Vill du installera den nu?</translation>
+    </message>
+</context>
+</TS>
